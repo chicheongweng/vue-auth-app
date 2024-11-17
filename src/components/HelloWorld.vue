b0e81240b61aa11c76094d67edf058ef740212ab
Chi-Cheong Weng <chicheongweng@gmail.com>
2024-11-17 10:44:54 +0800

    update HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,64 +2,71 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Welcome to our ESG (Environmental, Social, and Governance) platform. Here, we are committed to providing transparent and comprehensive ESG data.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Our ESG Focus Areas</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
+      <li>Environmental Sustainability</li>
+      <li>Social Responsibility</li>
+      <li>Governance and Ethics</li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3>Key Metrics</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li>Carbon Emissions</li>
+      <li>Water Usage</li>
+      <li>Employee Satisfaction</li>
+      <li>Community Engagement</li>
+      <li>Board Diversity</li>
+      <li>Ethical Compliance</li>
     </ul>
-    <h3>Ecosystem</h3>
+    <h3>Learn More</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="https://www.un.org/sustainabledevelopment/sustainable-development-goals/" target="_blank" rel="noopener">UN Sustainable Development Goals</a></li>
+      <li><a href="https://www.globalreporting.org/" target="_blank" rel="noopener">Global Reporting Initiative</a></li>
+      <li><a href="https://www.sasb.org/" target="_blank" rel="noopener">SASB Standards</a></li>
+      <li><a href="https://www.cdp.net/en" target="_blank" rel="noopener">CDP</a></li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { defineComponent } from 'vue';
 
-@Options({
+export default defineComponent({
+  name: 'HelloWorld',
   props: {
-    msg: String
+    msg: {
+      type: String,
+      required: true
+    }
   }
-})
-export default class HelloWorld extends Vue {
-  msg!: string
-}
+});
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello {
+  padding: 20px;
 }
+
+h1 {
+  color: #42b983;
+}
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
-  display: inline-block;
-  margin: 0 10px;
+  margin: 5px 0;
 }
+
 a {
   color: #42b983;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
 }
 </style>
